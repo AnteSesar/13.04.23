12f58790e247300f11584ea702e02cc592609ed5
Ante <ante.sesar11@gmail.com>
2023-04-17 19:21:27 +0200

    end

--- aplikacija1/src/components/App.js
+++ aplikacija1/src/components/App.js
@@ -2,16 +2,25 @@ import logo from '../assets/images/logo.svg';
 import '../assets/styles/App.css';
 
 import randomstring from 'randomstring';
+import { pi, sum, imeAplikacije as mojaVariabla, num2, num1, oduzmi } from './utilities';
+import person from './utilities/Person';
+
+// import Person from "./utilities/Person";
+// import { sum, pi, imeAplikacije as mojaVariabla} from "./utilities/Utility";
+
+
 
 function App() {
+  var zbroj = sum(10, 12) * pi;
   var rndStr = randomstring.generate();
   console.log(rndStr);
+  var od = oduzmi(num1, num2);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Moja React aplikacija {rndStr} 
+          {mojaVariabla}, {rndStr}, {person.name}, {zbroj}, {od}
         </p>
         <a
           className="App-link"
