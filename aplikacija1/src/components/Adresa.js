4efc7c0a8fb0553c6805d94101a842e28e90cfaf
Ante <ante.sesar11@gmail.com>
2023-05-11 16:57:20 +0200

    start

---
+++ aplikacija1/src/components/Adresa.js
@@ -0,0 +1,10 @@
+const Adresa = () => {
+    return(
+        <div>
+            <h2>Adresa</h2>
+            <p>Zagrab - Ilica 242, Maks (test)</p>
+        </div>
+    );
+}
+
+export default Adresa;
---
+++ aplikacija1/src/components/AppRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './Home';
+import { BrowserRouter } from 'react-router-dom';
+import Adresa from './Adresa';
+
+const Kontakt = () => {
+
+}
+
+export default function AppRouter() {
+    return(
+        <BrowserRouter>
+        </BrowserRouter>
+    )
+}
