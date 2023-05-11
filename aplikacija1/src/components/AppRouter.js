b457b140a059907c5cc216f0b8357f31eaf3857a
Ante <ante.sesar11@gmail.com>
2023-05-11 17:34:08 +0200

    end

--- aplikacija1/src/components/AppRouter.js
+++ aplikacija1/src/components/AppRouter.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import Home from './Home';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Adresa from './Adresa';
 
 const Kontakt = () => {
-
+    return(
+        <div>
+            <h2>Kontakt</h2>
+            <p>099 152 84 32</p>
+        </div>
+    );
 }
 
 export default function AppRouter() {
     return(
         <BrowserRouter>
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/adresa/">Adresa</Link>
+                    </li>
+                    <li>
+                        <Link to="/kontakt/">Kontakt</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" exact element={<Home/>} />
+                <Route path="/adresa/" element={<Adresa/>} />
+                <Route path="/kontakt/" element={<Kontakt/>} />
+            </Routes>
+        </div>
         </BrowserRouter>
     )
 }
--- aplikacija1/src/index.js
+++ aplikacija1/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+import AppRouter from './components/AppRouter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<AppRouter />);
